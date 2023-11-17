c2adacc647a3a38233216bdc8fa421e99746a468
sajid360 <ahteshamsajid8@gmail.com>
2023-11-18 02:30:36 +0600

    menu post, update and delete related api added

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ async function run() {
 
     // middleware
     const verifyToken = (req, res, next) => {
-      console.log('inside verify token', req.headers.authorization);
+      // console.log('inside verify token', req.headers.authorization);
       if (!req.headers.authorization) {
         return res.status(401).send({ message: "access unauthorized" })
       }
@@ -121,6 +121,46 @@ async function run() {
       const menu = await menuCollection.find().toArray()
       res.send(menu)
     })
+
+    app.post('/menu', verifyToken, verifyAdmin, async(req, res) =>{
+      const item = req.body;
+      const result = await menuCollection.insertOne(item)
+      res.send(result)
+    })
+
+    app.get('/menu/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result = await menuCollection.findOne(query)
+      res.send(result)
+    })
+
+    app.patch('/menu/:id', async(req, res) =>{
+      const item = req.body;
+      const id = req.params.id;
+      const filter = {_id : new ObjectId(id)}
+      const updatedDoc = {
+        $set : {
+          name: item.name,
+          category : item.category,
+          price : item.price,
+          image : item.image,
+          recipe : item.recipe
+        }
+      }
+
+      const result = await menuCollection.updateOne(filter, updatedDoc)
+      res.send(result)
+    })
+
+    app.delete('/menu/:id', verifyToken, verifyAdmin, async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result = await menuCollection.deleteOne(query)
+      res.send(result);
+    })
+
+    // reviews related api
     app.get('/reviews', async (req, res) => {
       const menu = await reviewCollection.find().toArray()
       res.send(menu)
